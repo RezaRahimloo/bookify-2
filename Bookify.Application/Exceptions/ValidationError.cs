608754068285f20317a30580beca9758a92de6fb
Reza Rahimloo <mr.rezarahimlou@gmail.com>
2026-07-28 13:00:35 +0330

    final touch

--- Bookify.Application/Exceptions/ValidationError.cs
+++ Bookify.Application/Exceptions/ValidationError.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Bookify.Application.Abstractions.Behaviors
+namespace Bookify.Application.Exceptions
 {
     public sealed record ValidationError(string PropertyName, string ErrorMessage);
 }
--- Bookify.Application/Exceptions/ValidationException.cs
+++ Bookify.Application/Exceptions/ValidationException.cs
@@ -1,5 +1,4 @@
-﻿using Bookify.Application.Abstractions.Behaviors;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
